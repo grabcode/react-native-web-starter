125fc17e47194a0dd82cf4d06918269c33fb8aef
Robb Lovell <robblovell@gmail.com>
2016-08-18 17:15:40 -0700

    removed chrome support. Moved chrome example to RN4All.

--- chrome/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@kadira/storybook';
-
-const req = require.context('../../app/stories', true)
-
-function loadStories() {
-  req.keys().forEach(req)
-}
-
-configure(loadStories, module);
